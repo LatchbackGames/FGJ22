005744d05f13b61098ace29eba01279818a81a16
jeiei <ahonju@gmail.com>
2022-01-29 19:50:28 +0200

    Animations added for main char

--- FGJ22/Assets/Scripts/Movement.cs
+++ FGJ22/Assets/Scripts/Movement.cs
@@ -8,8 +8,10 @@ public class Movement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
-    
+    private float moveX = 1;
+    private float dir = 0;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +19,26 @@ public class Movement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
+        //animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement.x == 0 || moveX == movement.x)
+            return;
+        
+        moveX = movement.x;
+        animator.SetFloat("Direction", moveX);
+        if (animator.GetFloat("Direction") == -1 && animator.GetFloat("Speed") > 0)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            
+        }
+        if (animator.GetFloat("Direction") != -1 && animator.GetFloat("Speed") > 0)
+        {
+            animator.SetFloat("Horizontal", 1);
+            
+        }
+        if(animator.GetFloat("Speed") == 0)
+            animator.SetFloat("Horizontal", 0);
+
         //TODO ANIMATOR FROM TOP DOWN MOVEMENT IN UNIY VIDEO
 
     }
@@ -24,5 +46,6 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
